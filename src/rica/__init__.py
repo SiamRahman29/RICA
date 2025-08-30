82d1fdb9c183a4040248b6a9b30692117f15b762
Siam Rahman <siam@graduate.utm.my>
2025-08-30 20:01:44 +0600

    fix; remove redundancy

--- src/rica/__init__.py
+++ src/rica/__init__.py
@@ -4,7 +4,3 @@ __version__ = "0.1.0"
 __author__ = "Siam Rahman"
 __email__ = "siam@graduate.utm.my"
 
-from .core.assistant import RICA
-from .core.agent_manager import AgentManager
-
-__all__ = ["RICA", "AgentManager"]
