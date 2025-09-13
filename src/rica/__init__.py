43e2612a478f2fd596dab6d8f25c9bfde206d1cb
Siam Rahman <siam@graduate.utm.my>
2025-09-13 22:45:32 +0600

    thousandth reorg

--- src/rica/__init__.py
+++
@@ -1,6 +0,0 @@
-"""RICA - A Rather Intelligent Conversational Assistant."""
-
-__version__ = "0.1.0"
-__author__ = "Siam Rahman"
-__email__ = "siam@graduate.utm.my"
-
--- src/rica/core/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Core functionality for the RICA."""
-
-from .assistant import RICA
-from .agent_manager import AgentManager
-from .config import Config
-
-__all__ = ["RICA", "AgentManager", "Config"]
